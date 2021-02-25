646e516573b9829bef2971da5c4cb500f230cfe8
Charles Suresh <emailcharlesjosh@gmail.com>
2021-02-25 10:58:57 +0530

    added unit tests for catbind

--- tests/test_fivetwofour_assignment_1.py
+++ tests/test_fivetwofour_assignment_1.py
@@ -1,5 +1,13 @@
 from fivetwofour_assignment_1 import __version__
 from fivetwofour_assignment_1 import fivetwofour_assignment_1
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((fivetwofour_assignment_1.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((fivetwofour_assignment_1.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
